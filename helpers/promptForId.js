0874224478e723cd8c6dfc9a221b5c143add3067
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-12 12:03:09 -0500

    rework the helper function to use an array of name/value objects for prompts

--- helpers/promptForId.js
+++ helpers/promptForId.js
@@ -5,25 +5,24 @@ const inquirer = require("inquirer");
 // Returns a promise that, when fulfilled, returns the id corresponding to the item the user chooses.
 function promptForId(db, idField, otherField, alias, table, prompt, extraChoices = []) {
     // Select ids and another column with an alias from the table
-    // (Prepared statements weren't working as expected here for some reason. The template literal is probably safe enough since this function isn't user-facing, at least in the way I'm using it)
+    // (Prepared statements weren't working as expected here for some reason, so I'm using a template literal)
     return db.query(`SELECT ${idField}, ${otherField} AS ${alias} FROM ${table}`)
     .then((result) => {
-        // Get the list of items from the query result
-        let itemsList = result[0].map((x) => `${x[alias]} (id:${x[idField]})`);
+        // Get the list of items and their associated ids from the query result
+        let promptsList = result[0].map((x) => {return {name: x[alias], value: x[idField]}});
+        console.log(promptsList);
         // Prompt the user for the item
         return inquirer.prompt([
             {
                 type: "list",
                 name: "item",
                 message: prompt,
-                choices: extraChoices.concat(itemsList)
+                choices: extraChoices.concat(promptsList)
             }
         ]);
     }).then(({item}) => {
-        // Find the item id for the query. If the item does not have an id, return null.
-        // (Regex selects the sequence of numbers preceding the ) that precedes the end of the input)
-        let itemMatch = item.match(/\d*(?=\)(?=$))/);
-        return (itemMatch) ? itemMatch[0] : itemMatch;
+        // Return the id associated with the item the user selected
+        return item;
     });
 }
 
