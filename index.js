65ec3e93d3f182284f66a2b51c624eb509581042
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-12 16:37:45 -0500

    add code to display ASCII-art title

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const CLI = require("./lib/cli.js");
+const figlet = require("figlet");
 
 // Create a new CLI (Command Line Interface object)
 const cli = new CLI();
+
+// Print title
+console.log(figlet.textSync(
+    "Employee Tracker",
+    {
+        font: "rectangles",
+        horizontalLayout: "default",
+        verticalLayout: "full",
+        width: 80,
+        whitespaceBreak: true
+    }
+), "\n");
 
 // Run the tool
 cli.chooseAction();
